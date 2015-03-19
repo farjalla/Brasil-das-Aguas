91f342c163e5edd5b3fcbf2abf878b95170a4a05
farjalla <farjalla@biologia.ufrj.br>
2015-03-19 11:52:57 -0700

    2015-03-19

---
+++ BrasAguas.R
@@ -0,0 +1,24 @@
+#iniciando analise dos dados do Brasil das aguas
+#sera utilizado como exercicio para meu avanco no uso do R e do GitHub
+
+library(nlme)
+library(ggplot2)
+library(dplyr)
+library(magrittr)
+library(tidyr)
+
+brasil<-read.table("C:/Users//Farjala/Documents/R/Brasil-das-Aguas/BrasAgua.txt", header=TRUE, stringsAsFactors = FALSE)
+str(brasil)
+as.numeric(as.character(brasil$Temp))
+as.numeric(as.character(brasil$OD))
+as.numeric(as.character(brasil$pH))
+
+# olhada geral na distribuicao dos dados ----------------------------------
+
+hist (brasil$Bacteria)
+hist (log(brasil$Bacteria))
+qqnorm (log(brasil$Bacteria))
+ks.test (log(brasil$Bacteria), pnorm)
+shapiro.test (log(brasil$Bacteria))
+
+# ambos Shapiro e K-S indicaram que mesmo os dados logaritmzados não são normais
